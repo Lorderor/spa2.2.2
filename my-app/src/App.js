f30da3202df05d232724aa1dee1886558755e661
Lorderor <gosha_ppp@bk.ru>
2018-12-06 02:02:53 +0300

    ref

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,82 @@
 import React, { Component } from 'react';
-import Tap from './Tap.js';
-
-
+import axios from "axios";
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            libData:[],
+            valueInput:0,
+            currentId:{},
+            showEl:false,
+            showElT:false
+        };
+    }
+    onLoadClick=()=>{
+        this.getReqFun();
+        this.setState({showElT:false});
+        this.setState({showEl:true})
+    };
+    onLoadClickId=()=>{
+        this.getReqIdFun();
+        this.setState({showEl:false});
+        this.setState({showElT:true})
+    };
+    onLoadCallbackId=(data)=>{
+        this.setState({currentId:data})
+    };
+    onLoadCallback=(data)=>{
+
+        this.setState({libData:data});
+    };
+    getReqFun = () => {
+        axios.get('http://localhost:8080/api/books')
+            .then(res => {
+                this.onLoadCallback(res.data);
+                console.log(res.data);
+            })
+            .catch(error=>{console.log(error)
+            });
+    };
+    getReqIdFun = () => {
+        axios.get('http://localhost:8080/api/book/'+this.state.valueInput)
+            .then(res => {
+                this.onLoadCallbackId(res.data);
+                console.log(res.data);
+            })
+            .catch(error=>{console.log(error)
+            });
+    };
+
+    render(){
+        const{id:idBook,name:nameBook, discription:discriptionBook} = this.state.currentId;
+        return (
+            <div>
+                <button onClick={this.onLoadClick}>Libs</button>
 
 
-  render(){
-    return (
-            <Tap />
-    );
-  }
+
+                <input type= "text" onChange={(evt)=> {
+                    this.setState({valueInput: evt.target.value})
+                }}/>
+
+                <button onClick={this.onLoadClickId} >Lib id</button>
+
+                <div>{
+                    this.state.showElT &&
+                    <div>
+                        <p>Номер в каталоге:{idBook}</p>
+                        <p>Название:{nameBook}</p>
+                        <p>Описание:{discriptionBook}</p>
+
+                    </div>
+                }
+                    {this.state.showEl && this.state.libData.map((item)=> (
+                        <div>Номер: {item.id}|Название: {item.name}|Описание: {item.discription}</div>
+                    ))
+                    }</div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- server/dist/server/serv.js
+++ server/dist/server/serv.js
@@ -12,7 +12,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
-var jsons = require('./lib.json');
+var jsons = require('../lib.json');
 // app.get('/',(req,res)=>{
 //     res.send();
 // });
